6dc2aeeb037af3e63da4aa24285f963747a8275c
Alan Mejia <mejiamartinezalan@gmail.com>
2022-01-18 10:44:39 -0600

    Eureka Config Finished

--- user-service/src/main/java/com/doctor/api/ApiApplication.java
+++ user-service/src/main/java/com/doctor/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.doctor.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiApplication {
 
 	public static void main(String[] args) {
